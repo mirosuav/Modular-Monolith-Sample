f6eb615750259407446f12c5da990f2f45c85a9f
Mirek <mirek@able.tech>
2024-09-13 10:25:39 +0200

    Tests for resultable

---
+++ src/SharedKernel.Tests/Helpers/ResultableTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using RiverBooks.SharedKernel.Helpers;
+
+namespace RiverBooks.SharedKernel.Tests.Helpers;
+
+public class ResultableTests
+{
+    [Fact]
+    public void Resultable_ShouldBeSerializable()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+
+        // Act
+        var sut = Resultable.Success(id);
+        var json = JsonSerializer.Serialize(sut);
+        var recreated = JsonSerializer.Deserialize<Resultable<Guid>>(json);
+
+        // Assert
+        recreated.Should().Be(sut);
+    }
+}
+
